226e0977016dc7d0386b645aa626eedd1adeaf51
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-13 22:15:01 -0300

    feature(backend): Implementa método responsável por realizar o encode da senha do usuário

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -3,6 +3,7 @@ package api.services;
 import api.exceptions.ApiException;
 import api.models.User;
 import api.repositories.UserRepository;
+import api.utils.EncoderUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final EncoderUtil encoderUtil;
     private static final String EMAIL_NOT_UNIQUE_MESSAGE = "O e-mail informado já está sendo utilizado por outra conta.";
+
+    public User register(User user) {
+        this.validateEmailUniqueness(user.getEmail());
+        this.encodeUserPassword(user);
+        return this.create(user);
+    }
 
     public User create(User user) {
         user.setId(null);
         return this.userRepository.save(user);
+    }
+
+    public void encodeUserPassword(User user) {
+        user.setPassword(this.encoderUtil.encode(user.getPassword()));
     }
 
     public Boolean existsByEmail(String email) {
