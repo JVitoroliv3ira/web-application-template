07536559e18f17d6e3309be070f70c8d6f1001c8
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-13 21:57:06 -0300

    feature(backend): Implementa método responsável por cadastrar um novo usuario

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,5 +1,6 @@
 package api.services;
 
+import api.models.User;
 import api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    public User create(User user) {
+        user.setId(null);
+        return this.userRepository.save(user);
+    }
 }
