39873b178fd155b308498f5efaf3cbbee2f5c800
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-13 21:10:37 -0300

    feature(backend): Cria controller de autenticacao da API

---
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -0,0 +1,11 @@
+package api.controllers.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping(path = "/api/v1/auth")
+@RestController
+public class AuthController {
+
+}
