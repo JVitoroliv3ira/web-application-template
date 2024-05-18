1f6b18efce9efede7670ffa30d41996c95dbe019
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-18 20:50:45 -0300

    feature(frontend): Faz o map entre os campos do formulário e o dto da request

--- web/src/app/modules/authentication/pages/page-register/page-register.component.ts
+++ web/src/app/modules/authentication/pages/page-register/page-register.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { RegisterRequestDTO } from 'src/app/core/dtos/requests/register-request.dto';
+
 @Component({
   selector: 'app-page-register',
   templateUrl: './page-register.component.html'
 })
 export class PageRegisterComponent {
+  public request: RegisterRequestDTO = {} as RegisterRequestDTO;
 
+  public handleRegisterClick(): void {
+    console.log(this.request);
+  }
 }
+
