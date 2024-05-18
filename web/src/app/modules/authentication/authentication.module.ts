74d34326265cbeb431a226655f4347ccbc3d762d
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-18 20:02:31 -0300

    feature(frontend): Adiciona o modulo Shared no modulo de autenticacao

--- web/src/app/modules/authentication/authentication.module.ts
+++ web/src/app/modules/authentication/authentication.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { PageRegisterComponent } from './pages/page-register/page-register.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -11,6 +12,7 @@ import { PageRegisterComponent } from './pages/page-register/page-register.compo
   ],
   imports: [
     CommonModule,
+    SharedModule,
     AuthenticationRoutingModule
   ]
 })
