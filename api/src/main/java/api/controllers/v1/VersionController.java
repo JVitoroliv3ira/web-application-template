d5300b1a4db275b0f5b847ef5441470f3ee7b1e9
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-19 16:44:17 -0300

    feature(backend): Adiciona anotacao @GetMapping no endpoint

--- api/src/main/java/api/controllers/v1/VersionController.java
+++ api/src/main/java/api/controllers/v1/VersionController.java
@@ -4,12 +4,15 @@ import api.dtos.responses.ResponseDTO;
 import api.dtos.responses.VersionResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(path = "/api/v1/version")
 @RestController
 public class VersionController {
+
+    @GetMapping
     public ResponseEntity<ResponseDTO<VersionResponseDTO>> aplicationVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
