3d90c5a8b70fed66bea148fe388b5057ab9eb58f
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-18 21:58:20 -0300

    feature(frontend): Implementa metodo responsavel pela validacao da request do usuario

--- web/src/app/modules/authentication/pages/page-register/page-register.component.ts
+++ web/src/app/modules/authentication/pages/page-register/page-register.component.ts
@@ -8,9 +8,34 @@ import { RegisterRequestDTO } from 'src/app/core/dtos/requests/register-request.
 })
 export class PageRegisterComponent {
   public request: RegisterRequestDTO = {} as RegisterRequestDTO;
+  public errors: string[] = []
+
 
   public handleRegisterClick(): void {
-    console.log(this.request);
+    this.errors = [];
+    this.validateRequest();
+  }
+
+  private validateRequest(): void {
+    const { name, email, password } = this.request;
+
+    if (name === null || name === undefined) {
+      this.errors.push('Informe o seu nome.');
+    } else if (name.trim().length < 5 || name.trim().length > 80) {
+      this.errors.push('O nome deve conter entre 5 e 80 caracteres.');
+    }
+
+    if (email === null || email === undefined) {
+      this.errors.push('Informe o seu e-mail.');
+    } else if (email.trim().length < 9 || email.trim().length > 90) {
+      this.errors.push('O e-mail deve conter entre 9 e 90 caracteres.');
+    }
+
+    if (password === null || password === undefined) {
+      this.errors.push('Informe a sua senha.');
+    } else if (password.trim().length < 6) {
+      this.errors.push('A senha deve conter pelo menos 6 caracteres.');
+    }
   }
 }
 
