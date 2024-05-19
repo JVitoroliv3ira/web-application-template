4db98d536075bea741f649b8a5da3b743a2040fe
João Oliveira <jvitoroliveira0711@gmail.com>
2024-05-19 18:24:15 -0300

    feature(frontend): Implementa funcoes para setar e recuperar a versao da aplicacao

--- web/src/app/core/services/version/version.service.ts
+++ web/src/app/core/services/version/version.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { ApiResponseDTO } from 'src/app/core/dtos/responses/api-response.dto';
 import { VersionResponseDTO } from 'src/app/core/dtos/responses/version-response.dto';
-import { Observable } from 'rxjs';
+import { get, set } from 'src/app/core/utils/localstorage-util';
+
+import { environment } from 'src/environments/environment';
+
+const APPLICATION_VERSION_KEY = 'APPLICATION_VERSION';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VersionService {
+  private apiBaseUrl: string = environment.apiBaseUrl;
+
   constructor(private http: HttpClient) {}
 
   public getVersion(): Observable<ApiResponseDTO<VersionResponseDTO>> {
     return this.http.get<ApiResponseDTO<VersionResponseDTO>>(
-      'http://localhost:8080/api/v1/version'
+      `${this.apiBaseUrl}/version`
     );
+  }
+
+  public setApplicationVersion(version: string): void {
+    set<string>(APPLICATION_VERSION_KEY, version);
+  }
+
+  public getApplicationVersion(): string | null {
+    return get<string>(APPLICATION_VERSION_KEY);
   }
 }
 
