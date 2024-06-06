d58dfef475ab351947594aa947157b4e1025f335
João Oliveira <jvitoroliveira0711@gmail.com>
2024-06-05 21:20:29 -0300

    feature(backend): Implementa a interface UserDetailsService na classe UserService

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,19 +1,23 @@
 package api.services;
 
 import api.exceptions.ApiException;
+import api.models.Details;
 import api.models.User;
 import api.repositories.UserRepository;
 import api.utils.EncoderUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final EncoderUtil encoderUtil;
     private static final String EMAIL_NOT_UNIQUE_MESSAGE = "O e-mail informado já está sendo utilizado por outra conta.";
+    private static final String USER_NOT_EXISTS_MESSAGE = "Usuário não encontrado na base de dados.";
 
     public User register(User user) {
         this.validateEmailUniqueness(user.getEmail());
@@ -41,5 +45,15 @@ public class UserService {
     public User create(User user) {
         user.setId(null);
         return this.userRepository.save(user);
+    }
+
+    @Override
+    public Details loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(email)
+                .orElseThrow(() -> new ApiException(
+                        USER_NOT_EXISTS_MESSAGE,
+                        HttpStatus.BAD_REQUEST
+                ));
+        return new Details(user);
     }
 }
